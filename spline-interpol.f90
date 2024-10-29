06cff2b7af79a5655382558844950464b244c885
Bijit Mukherjee <bijit.9791@gmail.com>
2024-10-29 11:43:22 +0100

    Interpolation can now be done with either new dx or new ngrid.

--- spline-interpol.f90
+++ spline-interpol.f90
@@ -2,24 +2,41 @@
       implicit none
       integer, parameter :: mxgrid = 10000, mxcol = 10
       character(100) :: fbasename, fileinput, fileoutput, line
-      character(3)   :: fextensn
-      integer :: nignore, nycol, ngrid
+      character(3)   :: fextnsion
+      integer :: nignore, nycol, ngrid, ngrid_new, igrid
       integer :: iline, icol
       real*8  :: x(mxgrid), y(mxcol,mxgrid), y2(mxcol,mxgrid)
+      real*8  :: xfirst, xlast
       real*8  :: xmin, xmax, dx_new, x_new, y_new(mxcol)
 
-      namelist/params/fbasename,fextensn,nignore,nycol,dx_new
+      namelist/params/fbasename,fextnsion,nignore,nycol,dx_new, &
+                      xfirst,xlast,ngrid_new
 
       ! Default values
-      nignore = 0
-      nycol = 1
-      dx_new  = 0.1d0
+      nignore = 0       ! no of lines containing text at the beginning of the file
+      nycol   = 1       ! no of data columns
+      dx_new  = -9999d0 ! new dx
+      ngrid_new = -9999 ! new ngrid
+      xfirst  = -9999d0 ! first data point
+      xlast   = -9999d0 ! last data point
 
       read(5,params)
       write(*,*)"This is program for spline interpolation"
+      write(*,*)
 
-      fileinput=trim(fbasename)//'.'//trim(fextensn)
-      fileoutput=trim(fbasename)//'_interpolated.'//trim(fextensn)
+      if (dx_new==-9999d0.and.ngrid_new==-9999) &
+              stop "Provide either dx_new or ngrid_new"
+
+      if (dx_new/=-9999d0.and.ngrid_new/=-9999) then
+         print*, "Both dx_new and ngrid_new are provided: ", &
+         "ngrid_new will be ignored"
+         ngrid_new=-9999
+      endif
+
+      if (ngrid_new==0) stop "Cannot have ngrid_new = 0"
+
+      fileinput=trim(fbasename)//'.'//trim(fextnsion)
+      fileoutput=trim(fbasename)//'_interpolated.'//trim(fextnsion)
 
       open(1,file=fileinput,status='old')
       open(10,file=fileoutput,status='unknown')
@@ -41,7 +58,20 @@
  1    continue
       close(1)
 
+      xmin = min(x(1),x(ngrid))
+      xmax = max(x(1),x(ngrid))
+
+      if (ngrid==0) stop "No data point provided"
+
       if (ngrid>mxgrid) stop "ngrid larger than mxgrid"
+
+      if (xfirst/=-9999d0.and.(xfirst<xmin.or.xfirst>xmax)) &
+              stop "xfirst outside range"
+      if (xlast/=-9999d0.and.(xlast<xmin.or.xlast>xmax)) &
+              stop "xlast outside range"
+
+      if (xfirst==-9999d0) xfirst = xmin
+      if (xlast ==-9999d0) xlast  = xmax
 
       write(*,*) "No. of input data points:",ngrid
 
@@ -49,16 +79,18 @@
         call spline(x(:ngrid),y(icol,:ngrid),ngrid,1.d30,1.d30,y2(icol,:ngrid))
       enddo
 
-      xmin = min(x(1),x(ngrid))
-      xmax = max(x(1),x(ngrid))
+      if (dx_new==-9999d0.and.ngrid_new>1) dx_new = (xfirst-xlast)/dble(ngrid_new-1)
+      if (ngrid_new==1) dx_new=0d0
+      if (ngrid_new==-9999) ngrid_new = int((xfirst-xlast)/dx_new)+1
 
-      x_new=xmin
-      do while (x_new<=xmax)
+      write(*,*) "No. of new data points:",ngrid_new
+
+      do igrid = 1, ngrid_new
+        x_new = xfirst + dble(igrid-1)*dx_new
         do icol=1,nycol
           call splint(x(:ngrid),y(icol,:ngrid),y2(icol,:ngrid),ngrid,x_new,y_new(icol))
         enddo
-        write(10,'(g14.4,10es15.6)')x_new, y_new(:nycol)
-        x_new = x_new+dx_new
+        write(10,'(f18.8,10es15.6)')x_new, y_new(:nycol)
       enddo
       close(10)
 
